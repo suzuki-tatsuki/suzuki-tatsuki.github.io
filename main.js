0bb6aaeee8a01a898371553afe43fbb4d4e5dec3
Suzuki_Tatsuki <suzuki.tatsuki.20@shizuoka.ac.jp>
2023-02-27 05:14:06 +0900

    自分のポートフォリオ

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-//const h1 = document.getElementById("midasi")
-//h1.textContent = "overwritten"
+var history = [
+    '2021年10月 シャドバサークルに入る',
+    '2022年8月  留年が確定する',
+];
+
+let counter = 0;
+
+function addHist() {
+    if (counter < history.length) {
+        let text = document.getElementById("contents").value;
+        console.log(text);
+        counter++;
+    }
+}
